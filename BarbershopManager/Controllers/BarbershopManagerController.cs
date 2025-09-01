c5f1a10e68baee12f08865de68f8a8add9ea9235
carlatakagi <carlatakagi@gmail.com>
2025-08-31 20:11:42 -0400

    feat: setup

---
+++ BarbershopManager/Controllers/BarbershopManagerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BarbershopManager.Controllers
+{
+    [ApiController]
+    [Route("api/v1/revenue")]
+    public class BarbershopManagerController : ControllerBase
+    {
+        private readonly ILogger<BarbershopManagerController> _logger;
+
+        public BarbershopManagerController(ILogger<BarbershopManagerController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public string GetRevenue()
+        {
+
+            return "Barbershop Manager is running!";
+        }
+    }
+}
